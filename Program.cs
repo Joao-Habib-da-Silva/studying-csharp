b262fb78a66342f5df950e50a58a1e0dc80bac49
Joao Habib da Silva <contajh544@gmail.com>
2025-05-03 20:02:14 -0300

    learning a little about csharp

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program {
+	static void Main(string[]args) {
+		int[] numberarray = new int[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int result = 0;
+    foreach (int i in numberarray) {
+      if(i % 2 == 0) {
+        result += i;
+      }
+      else {
+        Console.WriteLine("");
+      }
+    }
+    Console.WriteLine(result);
+	}
+}
